24d86eb5be93346571a26c22835fbdb24b42ba3d
nextmillion <nextmillion@protonmail.com>
2021-10-01 00:28:53 -0400

    commit

---
+++ _next/static/SlBT5AQ8vktahVyUdwUIv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/919-c6bfa7ffb4da6ab56397.js","static/chunks/pages/index-1ac4cede1190a684f324.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
